f7095db53235fcadccae24e94cc2d9aa889ade98
Alex <alex.kyllo@gmail.com>
2021-12-26 22:29:56 -0800

    add __version__ string

--- src/kqlalchemy/__init__.py
+++ src/kqlalchemy/__init__.py
@@ -1,6 +1,7 @@
 """KQLAlchemy
 A SQLAlchemy dialect for Azure Data Explorer (Kusto).
 """
+__version__ = "0.1.0"
 from sqlalchemy.dialects import registry
 
 from .kql_dialect import KQLDialect, kusto_engine, kusto_table
